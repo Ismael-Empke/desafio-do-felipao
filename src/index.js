90e21694b90a16a9b17e9c95210d3a2467b36282
Ismael-Empke <ismaelempke@gmail.com>
2024-05-22 16:19:22 -0300

    my first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,68 @@
-console.log("Hello World")
+let herosName = "Chavinho"
+let lvl = 1920
+
+if (lvl <=1000) {
+
+    console.log("The hero who his name is" + herosName + "have the iron lvl")
+
+}
+
+else if (lvl >=1001 && lvl < 2000) {
+
+    console.log("The hero who his name is" + herosName + "have the bronze lvl")
+
+}
+
+else if (lvl >=2000 && lvl < 3000) {
+
+    console.log("The hero who his name is" + herosName + "have the prata lvl")
+
+}
+
+else if (lvl >=3000 && lvl < 4000) {
+
+    console.log("The hero who his name is" + herosName + "have the gold lvl")
+
+}
+
+else if (lvl >= 4000 && lvl < 5000 ) {
+
+    console.log("The hero who his name is" + herosName + "have the platina lvl")
+
+}
+
+else if (lvl >= 5000 && lvl < 6000) {
+
+    console.log("The hero who his name is" + herosName + "have the diamond lvl")
+
+}
+
+else if (lvl >= 6000 && lvl < 7000) {
+
+    console.log("The hero who his name is" + herosName + "have the master lvl")
+
+}
+
+else if (lvl >= 7000 && lvl < 8000) {
+
+    console.log("The hero who his name is" + herosName + "have the gran master lvl")
+
+}
+
+else if (lvl >= 8000 && lvl < 9000) {
+
+    console.log("The hero who his name is" + herosName + "have the professional lvl")
+
+}
+
+else if (lvl >= 9000 && lvl < 10000) {
+
+    console.log("The hero who his name is" + herosName + "have the legend lvl")
+
+}
+
+else if (lvl >= 10000) {
+
+    console.log("The hero who his name is" + herosName + "have the invencible lvl")
+
+}
